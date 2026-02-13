913690e2f6c66d4689fefbf15956dab8d9bc3d1c
gpmakm <esikshatutorialsakarsh@gmail.com>
2026-02-13 22:35:20 +0530

    UI upgraded

--- src/app/Navbar.jsx
+++ src/app/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
 const Navbar = () => {
+ 
   return (
     <nav>
-        <span>KASMA SERVICES</span>
-       
+      <span>KASMA SERVICES</span>
+      <ul style={{ display: 'flex' }} id='menuItems'>
+
+        <li> <a href="#Snacks">Snacks</a> </li>
+        <li> <a href="#Stationary">Cyber Cafe Services</a> </li>
+        <li> <a href="#Partners">Our partners</a> </li>
+
+      </ul>
     </nav>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -10,21 +10,32 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        
+       
         <div className="container" >
-         
+          <p>
             Welcome to Kasma Services! We are dedicated to providing you with
             top-notch <a href="#snacks">snacks (coming soon) </a>and <a href="#Stationary">cyber cafe stationary services</a>. Whether you're craving
             delicious meals or need essential stationery supplies, we've got you
             covered. Our user-friendly platform allows you to easily browse menus,place orders, and connect with local vendors. Experience convenience, quality, and reliability all in one place with Kasma Services. Your
             satisfaction is our priority!
-
-            <div  style={{display:'flex',flexDirection:'column'}}>
+</p>
+            <div className="code" style={{display:'flex',flexDirection:'column'}}>
               <code>Charges for snacks: Charges are mentioned but if you ordered for delievery then the prices will be increased from Rs.10 to Rs. 20</code>
             <code>Cyber cafe services will be delievered only at evening time</code>
             </div>
          
 
+
+<div className="partners" id="Partners">
+  <p>
+  <h3>Our partners</h3>
+  <ul>
+    <li>Sharma Cyber Cafe</li>
+    <li>Nandu hotel and mess</li>
+    
+  </ul>
+</p>
+</div>
         </div>
         
         <div id="Stationary">
