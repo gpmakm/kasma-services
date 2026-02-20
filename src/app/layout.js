1e386412888f5de058c4e58de22620b5ee6b1683
gpmakm <esikshatutorialsakarsh@gmail.com>
2026-02-20 23:07:03 +0530

    Updated2 adsense

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2797882078011980"
           crossOrigin="anonymous"
-        ></script>
+        />
       </head>
         <Navbar/>
         {children}
