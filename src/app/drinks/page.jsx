5a5166de9ecf35c4b8ca147cfb96608916cab172
gpmakm <esikshatutorialsakarsh@gmail.com>
2026-06-08 08:01:11 +0530

    Resolved issue of loading stationary items:

--- src/app/drinks/page.jsx
+++ src/app/drinks/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import drink from '../../../public/drinks.json'
 import Image from 'next/image'
-// import im from '../../../images/'
+
 const Drinks = () => {
   return (
     <div>
@@ -11,11 +11,13 @@ const Drinks = () => {
                 {
                     drink.map((drinks)=>{
                         return (
-                           <div className='drinkcard' key={drinks.id}>
+                           <div className='drinkcard' key={drinks.name}>
                              <div key={drinks.id}>
+                                {console.log(drinks.image)}
+                                
                                 <Image src={drinks.image} alt={drinks.name} width={200} height={200} />
                                 <h4>{drinks.name}</h4>
-                                <p>${drinks.price}</p>
+                                <p>{drinks.price}</p>
                             </div>
                            </div>
                         )
@@ -23,6 +25,7 @@ const Drinks = () => {
                 }
             </div>
         </div>
+        {/* <Image src={im}/> */}
         </div>
 
     
--- src/app/stationary-order/page.jsx
+++ src/app/stationary-order/page.jsx
@@ -5,17 +5,18 @@ import { React, useState } from 'react'
 const StItem = () => {
   const stationaryCart = [];
   const [username, setUserName] = useState("");
-  const [contact, setContact] = useState()
+  const [contact, setContact] = useState("")
   const [quantity, setQuantity] = useState(1)
   const [quantity2, setQuantity2] = useState(1)
   const [quantity3, setQuantity3] = useState(1)
   const [quantity4, setQuantity4] = useState(1)
-  const handlePenClick = (e) => { stationaryCart.push("Pen - " + quantity4); e.target.style = "text-decoration:line-through; content: ' (Added)' " }
-  const handleEraserClick = (e) => { stationaryCart.push("Eraser - " + quantity); e.target.style = "text-decoration:line-through" }
-  const handleSharpenerClick = (e) => { stationaryCart.push("Sharpener - " + quantity2); e.target.style = "text-decoration:line-through" }
-  const handleGluClick = (e) => { stationaryCart.push("Glue - " + quantity3); e.target.style = "text-decoration:line-through" }
-  const handleScissorsClick = (e) => { stationaryCart.push("Scissors - " + quantity4); e.target.style = "text-decoration:line-through" }
-  const handleNotebookClick = (e) => { stationaryCart.push("Notebook - " + quantity3); e.target.style = "text-decoration:line-through" }
+  const handlePenClick = (e) => { stationaryCart.push("Pen - " + quantity4); e.target.style = "text-decoration:line-through; content: ' (Added)' "; console.log(stationaryCart);
+   }
+  const handleEraserClick = (e) => { stationaryCart.push("Eraser - " + quantity); e.target.style = "text-decoration:line-through" ;  console.log(stationaryCart) }
+  const handleSharpenerClick = (e) => { stationaryCart.push("Sharpener - " + quantity2); e.target.style = "text-decoration:line-through" ;  console.log(stationaryCart) }
+  const handleGluClick = (e) => { stationaryCart.push("Glue - " + quantity3); e.target.style = "text-decoration:line-through" ;  console.log(stationaryCart) }
+  const handleScissorsClick = (e) => { stationaryCart.push("Scissors - " + quantity4); e.target.style = "text-decoration:line-through" ;  console.log(stationaryCart) }
+  const handleNotebookClick = (e) => { stationaryCart.push("Notebook - " + quantity3); e.target.style = "text-decoration:line-through" ;  console.log(stationaryCart) }
 // const cartItems = [];
 //   cartItems.push(stationaryCart);
      
@@ -25,9 +26,13 @@ const StItem = () => {
     
        const message = "Name: " + username + "\nContact: " + contact + "\nOrdered items: " + stationaryCart.join(", ");
     const encodedMessage = encodeURIComponent(message);
+    const message2 = "Name: " + username + "\nContact: " + contact + "\nOrdered items: " + stationaryCart.length;
+  //  console.log(message);
+  //  console.log(message2);
+   
    
     const whatsappURL = `https://wa.me/${process.env.NEXT_PUBLIC_PAY_MERCHANT}?text=${encodedMessage}`;
-    window.open(whatsappURL);
+   window.open(whatsappURL);
 
   }
   
@@ -39,8 +44,8 @@ const StItem = () => {
       <form onSubmit={handleSubmit}>
         <ul style={{ margin: 65, padding: 15, listStyleType: 'none', padding: 7 }}>
 
-          <input type="text" name="username" value={username} id="" className="text" placeholder='Enter your name' onChange={(e) => { setUserName(e.target.value) }} required />
-          <input type="number" name="phone" value={contact} id="" className="text" placeholder='Enter your phone number' onChange={(e) => { setContact(e.target.value) }} required />
+          <input type="text" name="username" value={username} id="" className="text" placeholder='Enter your phone number' onChange={(e) => { setUserName(e.target.value) }} required />
+          {/* <input type="text" name="phone" value={contact} id="" className="text" placeholder='Enter your phone number' onChange={(e) => { setContact(e.target.value) }} required /> */}
           <li> <button onClick={handlePenClick
           } type="button" >Pen</button>  <select value={quantity4} onChange={(e) => { setQuantity4(e.target.value) }}> <option value={1}>1</option> <option value={2}>2</option> <option value={3}>3</option> <option value={4}>4</option> <option value={5}>5</option> <option value={6}>6</option> <option value={7}>7</option> <option value={8}>8</option> <option value={9}>9</option> <option value={10}>10</option> </select> </li>
           <li> <button onClick={handleEraserClick} type="button"  > Eraser </button> <select value={quantity} onChange={(e) => { setQuantity(e.target.value) }}> <option value={1}>1</option> <option value={2}>2</option> <option value={3}>3</option> <option value={4}>4</option> <option value={5}>5</option> <option value={6}>6</option> <option value={7}>7</option> <option value={8}>8</option> <option value={9}>9</option> <option value={10}>10</option> </select> </li>
@@ -48,7 +53,7 @@ const StItem = () => {
           <li> <button onClick={handleGluClick} type="button" >Glue</button> </li>
           <li> <button onClick={handleScissorsClick} type="button" >Scissors</button> </li>
           <li> <button onClick={handleNotebookClick} type="button" >Notebook</button> <select value={quantity3} onChange={(e) => { setQuantity3(e.target.value) }}> <option value={1}>1</option> <option value={2}>2</option> <option value={3}>3</option> <option value={4}>4</option> <option value={5}>5</option> <option value={6}>6</option> <option value={7}>7</option> <option value={8}>8</option> <option value={9}>9</option> <option value={10}>10</option> </select> </li>
-          <code>Click to add an item</code>
+          <code>Click to add an item, firstly select the quantity then place item</code>
         </ul>
         <button> Place order </button>
       </form>
