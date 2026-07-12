7402e89587b57ef7193695d7f9874a192c61e943
gpmakm <esikshatutorialsakarsh@gmail.com>
2026-07-12 20:24:10 +0530

    Completed UI improved!!!

--- src/app/drinks/page.jsx
+++ src/app/drinks/page.jsx
@@ -15,12 +15,13 @@ const Drinks = () => {
                         return (
                            <div className='drinkcard' key={drinks.name}>
                              <div key={drinks.id}>
-                                //{console.log(drinks.image)}
+                                
                                 
                                 <Image src={drinks.image} alt={drinks.name} width={100} height={100} />
                                 <h4>{drinks.name}</h4>
                                 <p>{drinks.price}</p>
-                                 <button type='button'>Place order</button>
+                               <input type="number" name="quantity" placeholder='Enter quantity' />
+                                 <button type='button'>Add to cart</button>
                             </div>
                            </div>
                         )
