57901bd67d888ae97975c1be97f332ece3ab11ad
gpmakm <esikshatutorialsakarsh@gmail.com>
2026-07-12 20:13:39 +0530

    Completed UI part of drinks page

--- src/app/drinks/page.jsx
+++ src/app/drinks/page.jsx
@@ -15,11 +15,12 @@ const Drinks = () => {
                         return (
                            <div className='drinkcard' key={drinks.name}>
                              <div key={drinks.id}>
-                                {console.log(drinks.image)}
+                                //{console.log(drinks.image)}
                                 
-                                <Image src={drinks.image} alt={drinks.name} width={200} height={200} />
+                                <Image src={drinks.image} alt={drinks.name} width={100} height={100} />
                                 <h4>{drinks.name}</h4>
                                 <p>{drinks.price}</p>
+                                 <button type='button'>Place order</button>
                             </div>
                            </div>
                         )
