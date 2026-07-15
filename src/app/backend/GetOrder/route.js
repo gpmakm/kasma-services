230a0a5b47a7ae967cb4ce72b7f66d55a9eb05f7
gpmakm <esikshatutorialsakarsh@gmail.com>
2026-07-16 00:00:29 +0530

    order ready to receive as json file

--- src/app/backend/GetOrder/route.js
+++ src/app/backend/GetOrder/route.js
@@ -5,12 +5,13 @@ import path from 'path'
 
 export async function POST(req){
     let order=await req.json()
-    let filePath=path.join(process.cwd(),'order.json')
-    let data=fs.writeFileSync(filePath, JSON.stringify(order),'utf-8',(err)=>{
-        if(err){
-            console.log(err)
-            return NextResponse.json({message:'Error in writing data'}, {status:500})
-        } 
-    })
+    let filePath=path.join(process.cwd(),'/public/order.json')
+   try {
+    fs.writeFileSync(filePath, JSON.stringify(order), 'utf-8');
+    console.log("Data written successfully");
+} catch (err) {
+    console.error("Error writing file:", err);
+    return NextResponse.json({message:'Error in writing data'}, {status:500});
+}
     return NextResponse.json({message:'Data received and saved to file'})
 }
